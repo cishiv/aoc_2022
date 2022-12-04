9f17eb56e58072b2d0f5d7d43babe8f4dbb88290
Shivan <shivantheman@gmail.com>
2022-12-04 16:53:38 +0200

    day4

---
+++ src/day4.ts
@@ -0,0 +1,38 @@
+import { intersect, _Array } from "./utils/arrays";
+import { readInput } from "./utils/reader"
+
+const dayFour = () => {
+	readInput(4, (_: any, d: string) => {
+		const nn = d.split("\n")
+			.map((pair: string) => pair
+				.split(",")
+				.map((opts: string) => opts
+					.split("-")
+					.map((n: string) => Number(n))));
+		console.time("day_four_part_1");
+		const partOne = nn.map((p: number[][]) => {
+			return (p[0][0] >= p[1][0] && p[0][1] <= p[1][1]) || (p[0][0] <= p[1][0] && p[0][1] >= p[1][1]) ? 1 : 0;
+		}).reduce((a: number, g: number) => a + g, 0);
+		console.timeEnd("day_four_part_1");
+
+		console.time("day_four_part_2");
+		const partTwo = nn.map((p: number[][]) => {
+			return intersect(_Array.range(p[0][0], p[0][1], 1), _Array.range(p[1][0], p[1][1], 1)).length > 0 ? 1 : 0;
+		}).reduce((a: number, g: number) => a + g, 0);
+		console.timeEnd("day_four_part_2");
+
+		console.log("Part 1", partOne);
+		console.log("Part 2", partTwo);
+	});
+}
+
+dayFour();
+export {}
+
+// 5-7,7-9 
+// 2-8,3-7
+// 6-6,4-6
+// 2-6,4-8
+
+// 3-10, 5-15
+
--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,3 +1,15 @@
 export const partition = (array: string[], n: number): string[][] => {
 	return array.length ? [array.splice(0, n)].concat(partition(array, n)) : [];
-} 
+}
+
+export class _Array<T> extends Array<T> {
+	static range(from: number, to: number, step: number): number[] {
+		return Array.from(Array(Math.floor((to - from) / step) + 1)).map(
+			(v, k) => from + k * step
+		);
+	}
+}
+
+export const intersect = (a: number[], b: number[]): number[] => {
+	return a.filter((x) => b.includes(x));
+}
