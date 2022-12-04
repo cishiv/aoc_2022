fde9dc161c4de2939d7f3cd95ef851f625c96ff4
Shivan <shivantheman@gmail.com>
2022-12-04 17:00:13 +0200

    add a comment about sets

--- src/day4.ts
+++ src/day4.ts
@@ -15,6 +15,10 @@ const dayFour = () => {
 		}).reduce((a: number, g: number) => a + g, 0);
 		console.timeEnd("day_four_part_1");
 
+		// as a note, doing an intersect here isn't ideal for 2 reasons:
+		// 1. we're not dealing with sets strictly, but implicitly from the input
+		// 2. it's probably really slow on very large input
+		// part-1 esque comparison of ranges fits and would probably be faster. But this approach looks cool!
 		console.time("day_four_part_2");
 		const partTwo = nn.map((p: number[][]) => {
 			return intersect(_Array.range(p[0][0], p[0][1], 1), _Array.range(p[1][0], p[1][1], 1)).length > 0 ? 1 : 0;
