5444d1be6cd347a03260b60e61d29425826a0cb3
Shivan <shivantheman@gmail.com>
2022-12-06 11:15:30 +0200

    more succint, but slower using sets

--- src/day6.ts
+++ src/day6.ts
@@ -1,13 +1,14 @@
 import { readInput } from "./utils/reader"
 
-const daySix = () => {
+// Faster
+const daySixIterative = () => {
 	readInput(6, (_: any, d: string) => {
 		const seenBeforePacketMarkers: string[] = [];
 		let partOne = -1;
 
 		const signalParts = d.split("");
 
-		console.time("day_six_part_one");
+		console.time("day_six_part_one_i");
 		for(let i = 0; i < signalParts.length; i++) {
 			const part = signalParts[i];
 			if (!seenBeforePacketMarkers.includes(part) && seenBeforePacketMarkers.length < 4) {
@@ -23,12 +24,12 @@ const daySix = () => {
 			}
 		}
 		console.log("Part One", partOne);
-		console.timeEnd("day_six_part_one");
+		console.timeEnd("day_six_part_one_i");
 
 		const seenBeforeMessageMarkers: string[] = [];
 		let partTwo = -1;
 
-		console.time("day_six_part_two");
+		console.time("day_six_part_two_i");
 		for(let i = 0; i < signalParts.length; i++) {
 			const part = signalParts[i];
 			if (!seenBeforeMessageMarkers.includes(part) && seenBeforeMessageMarkers.length < 14) {
@@ -45,10 +46,33 @@ const daySix = () => {
 		}
 
 		console.log("Part Two", partTwo);
+		console.timeEnd("day_six_part_two_i");
+	});
+}
+
+// More succint
+const daySixSets = () => {
+	readInput(6, (_: any, d: string) => {
+
+		const findMarker = (input: string, size: number) => {
+			for (let i = 0; i < input.length; i++) {
+				const chars = input.slice(i, i + size);
+				if (new Set([...chars]).size === size) {
+					return i + size;
+				}
+			}
+			return -1;
+		}
+		console.time("day_six_part_one");
+		console.log("Part One", findMarker(d, 4));
+		console.timeEnd("day_six_part_one");
+
+		console.time("day_six_part_two");
+		console.log("Part Two", findMarker(d, 14));
 		console.timeEnd("day_six_part_two");
 	});
 }
 
-daySix()
-
+daySixIterative();
+daySixSets();
 export {}
