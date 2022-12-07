0c81f2c0f5f804284a7a9ad8df536dcc94321eaa
Shivan <shivantheman@gmail.com>
2022-12-07 22:15:50 +0200

    day7, a bit of a mess, but its late

---
+++ src/day7.ts
@@ -0,0 +1,74 @@
+import { readInput } from "./utils/reader";
+
+const daySeven = () => {
+	readInput(7, (_: any, d: string) => {
+		const input = d.split("\n");
+		const path: string[] = [];
+		const fileSystem = new Map<string, string[]>();
+		let listing = false;
+		for (const line of input) {
+			if (line.includes("$ cd ") ) {
+				listing = false;
+				const directory = line.split("$ cd ")[1];
+				if (directory !== "..") {
+					path.push(directory);
+					continue;
+				}
+				else {
+					path.pop();
+					continue;
+				}
+			} else if (line === "$ ls") {
+				listing = true;
+				continue;
+			}
+
+			if (listing) {
+				const currentDirectory = fileSystem.get(path.join("/"));
+				if (currentDirectory) {
+					currentDirectory.push(line);
+				} else {
+					fileSystem.set(path.join("/"), [line]);
+				}
+			}
+		}
+
+		const sizes: number[] = []
+		fileSystem.forEach((value, key) => {
+			const size = directorySize(key, value, fileSystem)
+			sizes.push(size)
+		});
+
+		const partOne = sizes.sort((a, b) => b - a).filter((i, _) => i <= 100000).reduce((a, b) => a + b, 0);
+
+		console.log("Part One", partOne);
+    
+		const TOTAL_SPACE_AVAILABLE = 70000000;
+		const REQUIRED_SPACE = 30000000;
+		const unusedSpace = TOTAL_SPACE_AVAILABLE -  sizes.sort((a, b) => b - a)[0];
+		const spaceToClear = REQUIRED_SPACE - unusedSpace;
+
+		console.log("Part Two", sizes.filter((i, _) => i >= spaceToClear).sort((a, b) => a - b)[0]);
+	});
+
+}
+
+const directorySize = (source: string, contents: string[], fileSystem: Map<string, string[]>) => {
+	let size = 0;
+	for (const file of contents) {
+		if (file.includes("dir")) {
+			const directory = fileSystem.get(source + "/" + file.split(" ")[1]);
+			if (directory) {
+				size += directorySize(source + "/" + file.split(" ")[1], directory!, fileSystem);
+			}
+		} else {
+			size += parseInt(file.split(" ")[0]);
+		}
+	}
+	return size;
+}
+
+
+daySeven();
+
+export {}
